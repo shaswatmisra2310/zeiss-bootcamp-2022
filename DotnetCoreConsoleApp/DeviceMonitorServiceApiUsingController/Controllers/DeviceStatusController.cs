f4c76cbb11a5b266957f1e4355c6ce2dabfc987c
shaswatmisra2310 <43674831+shaswatmisra2310@users.noreply.github.com>
2022-09-13 13:12:55 +0530

    Update DeviceStatusController.cs

--- DotnetCoreConsoleApp/DeviceMonitorServiceApiUsingController/Controllers/DeviceStatusController.cs
+++ DotnetCoreConsoleApp/DeviceMonitorServiceApiUsingController/Controllers/DeviceStatusController.cs
@@ -16,7 +16,33 @@ namespace DeviceMonitorServiceApiUsingController.Controllers
             DeviceStatusMonitorLib.DeviceMonitor _monitorRef = new DeviceStatusMonitorLib.DeviceMonitor();
             return _monitorRef.GetDeviceStatusCode();
         }
+        [HttpGet("GetbyName/{name}")]
+        public int GetAllbyName()
+        {
+            
+        }
+        
+        [HttpPut("Put")]
 
+        public IActionResult Put([FromBody] UserRequest request)
+        {
+            Countries country = new Countries();
+            
+
+            try
+            {
+                _countryContext.countries.Add(country);
+                _countryContext.SaveChanges();
+
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "An error has occured");
+            }
+            
+            return Ok();
+
+        }
         
     }
 }
